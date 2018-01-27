748d25276d2922528fa45524972f244a543686a8
sambouzroud <sambouzroud@gmail.com>
2018-01-27 12:03:34 -0700

    first

---
+++ keys.js
@@ -0,0 +1,13 @@
+// console.log('This is loaded.');
+
+exports.twitterKeys = {
+    consumer_key: '	kuZETWRee7LAyvHLxKbtkzwBu',
+    consumer_secret: 'c4yduLLlmd9xyhLzSnhA4fYCqYlRxGO4TyOrm2v2xdiNF8IFwt',
+    access_token_key: '955206360878137344-jOCX1l3QiCBBQY9B3zJWu2TtbyNoKBC',
+    access_token_secret: 'HE4NB9DvyfRBJ2qYuXTvBXncZbhbyWynJ5eDIvmW3cCwW',
+};
+
+exports.spotifyKeys = {
+    id: '23fd138c2d4340c58cd6b29e4974a78c',
+    secret: 'bc64e0445be04781ab7c29231e4a73fa'
+}
